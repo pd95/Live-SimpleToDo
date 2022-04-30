4416bfd71cdf9a744fa15fbcd05f2149454e4222
Philipp <pd95@users.noreply.github.com>
2022-04-30 18:05:36 +0200

    Initial Commit

---
+++ SimpleToDo/SimpleToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoApp.swift
+//  SimpleToDo
+//
+//  Created by Philipp on 30.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
